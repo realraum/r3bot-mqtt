f231255d921d3d0b877685411256539e74f401db
stefan <dev@2904.cc>
2015-11-02 04:38:48 +0100

    added interesting events to zeromq filter

--- r3zmqfilter.py
+++ r3zmqfilter.py
@@ -1,14 +1,33 @@
+import ast
 
 # None filter:
-#class r3zmqfilter():
-#    def do(self, structname, structdata):
-#         return structname
+# class r3zmqfilter():
+#    def do(self, eventname, eventdata):
+#         return eventname
 #
 
 # Doom-Button filter:
-class r3zmqfilter():
-    def do(self, structname, structdata):
-        if structname == "BoreDoomButtonPressEvent":
-            return "Dooom! The button has been pressed! Propably someone is bored and in need of company! ;-)"
-        #return structname
 
+
+class r3zmqfilter():
+
+    def do(self, eventname, eventdata):
+        if eventname == "BoreDoomButtonPressEvent":
+            return "Dooom! The button has been pressed! Propably someone is bored and in need of company! ;-)"
+
+        #elif eventname == "TempSensorUpdate":
+        #    return "Temperature changed to " + str(eventdata['Value'])
+
+        elif eventname == "PresenceUpdate":
+            if eventdata['Present']:
+                return "Realraum now open! \o/"
+            else:
+                return "Realraum now closed ..."
+
+        elif eventname == "BackdoorAjarUpdate":
+            if eventdata['Shut']:
+                return "Backdoor closed."
+            else:
+                return "Backdoor opened."
+
+        #return eventname + ": " + str(eventdata)
